c3cb3614e2be0f5714ae290f43b28fba8d872419
savitha12 <80628303+savi1248@users.noreply.github.com>
2023-09-18 10:54:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 
 import Login from './Login';
+
 // import Senchola from './Senchola';
 import Why from './Why';
 
 function App() {
   return (
     <div className="App">
+      
       <Login />
       {/* <Senchola /> */}
       <Why />
